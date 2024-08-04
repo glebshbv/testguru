7c5cc597eafeb10a3db33ab68971a534db020202
Gleb <me@smoofy.co>
2024-08-04 21:49:54 +0800

    final commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
 
   def tests_by_level(level)
-    Test.joins("INNER JOIN results ON results.test_id = tests.id INNER JOIN users on results.user_id = users.id")
-        .where(level: level, users: { id: self.id })
-        .select("tests.*")
+    Test.joins("INNER JOIN results ON results.test_id = tests.id")
+        .where(tests: { level: level })
+        .where(results: { user_id: self.id })
   end
 end
