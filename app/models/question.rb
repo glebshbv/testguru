4c262f2f6cfbc225571fb4daf2f40dd658fdbe14
Gleb <me@smoofy.co>
2024-08-11 13:20:08 +0800

    Added Validations

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,15 @@ class Question < ApplicationRecord
   belongs_to :test
 
   has_many :answers, dependent: :destroy
+
+  validates :body, presence: true
+  validate :validate_answers_count
+
+  private
+
+  def validate_answers_count
+    if answers.count < 1 || answers.count > 4
+      errors.add(:answers, "У одного Вопроса может быть от 1-го до 4-х ответов" )
+    end
+  end
 end
