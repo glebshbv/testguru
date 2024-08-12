726ad3418ff453bf3c64cd820146f9114e6a883f
Gleb <me@smoofy.co>
2024-08-12 09:54:29 +0800

    added DB validation. Added answers count validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,16 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :body, presence: true
+  validate :validate_answers_count
 
   scope :correct, -> { where(correct: true) }
+
+  private
+
+  def validate_answers_count
+    if question.answers.count >= 4
+      errors.add(:question, "уже имеет максимальное количество ответов (4)")
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_02_052418) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_12_014120) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +55,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_02_052418) do
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
   end
 
   create_table "users", force: :cascade do |t|
