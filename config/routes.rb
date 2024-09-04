1006897fdbc6d18f531a1f386130b2c2974ec0a6
Gleb <gleb@adeblo.com>
2024-09-04 23:33:01 +0800

    Created MVC logic for Answers

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root "tests#index"
 
   resources :tests do
-    resources :questions, shallow: true, except: [:index]
+    resources :questions, shallow: true, except: [:index] do
+      resources :answers, shallow: true, except: %i[index]
+    end
   end
 
   get "up" => "rails/health#show", as: :rails_health_check
