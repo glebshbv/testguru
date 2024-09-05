311472b91cb25973afde96f993d269ec9ac93ee7
Gleb <gleb@adeblo.com>
2024-09-05 19:14:40 +0800

    only left after save

--- app/models/result.rb
+++ app/models/result.rb
@@ -4,7 +4,7 @@ class Result < ApplicationRecord
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
-  before_save :set_next_question
+
 
   def completed?
     current_question.nil?
@@ -14,16 +14,12 @@ class Result < ApplicationRecord
     if correct_answer?(answer_ids)
       self.correct_questions += 1
     end
-
+    self.current_question = next_question
     save!
 
   end
 
   private
-
-  def set_next_question
-    self.current_question = next_question
-  end
 
   def before_validation_set_first_question
     self.current_question = test.questions.first if test.present?
