301fe2a3c10fb88f560e61a24484536929ca391a
Gleb <me@glebshabanov.com>
2024-09-25 17:38:33 +0800

    fixed login

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :users, only: :create
 
   get :login, to: 'sessions#new'
+  post :login, to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy', as: :logout
 
 
